6c4221f719684206b935a3871fce4944ade94531
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-06-26 19:01:19 +0600

    push

--- clients/views.py
+++ clients/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, serializers
 from rest_framework.response import Response
 from rest_framework.decorators import action, api_view
 from django.db.models import Sum
